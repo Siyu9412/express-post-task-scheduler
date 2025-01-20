5b617a28317798fb78c47c412404258b6d5e8766
Leo Liu <leo.liu@sciplatforms.com>
2025-01-20 11:57:17 -0500

    update postinstall script

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,3 +1,8 @@
+if (process.env.CI) {
+  console.log("Skipping postinstall script in CI environment.");
+  process.exit(0);
+}
+
 import { execSync } from "child_process";
 
 execSync("npx prisma db push --schema ../prisma/schema/prisma");
